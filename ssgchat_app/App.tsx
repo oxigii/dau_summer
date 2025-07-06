4f52093b9f85564973efd61a58decfde0a4045f4
rladbeoeod <rladbeoeod@naver.com>
2025-07-06 22:00:34 +0900

    Add ssgchat_app project

--- ssgchat_app/App.tsx
+++ ssgchat_app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
+import { SafeAreaView } from 'react-native';
+import AuthAnonymousScreen from './src/screens/AuthAnonymousScreen';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <AuthAnonymousScreen />
+    </SafeAreaView>
   );
 }
