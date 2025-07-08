5634dcce23ea1dd7e5414a7be2ee0b1449aceb08
duqordl <128285087+duqordl@users.noreply.github.com>
2025-07-09 02:41:07 +0900

    역할1 추가 코드

--- ssgchat_app/App.tsx
+++ ssgchat_app/App.tsx
@@ -5,13 +5,21 @@ import AuthAnonymousScreen from './src/screens/AuthAnonymousScreen';
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [nickname, setNickname] = useState('');
 
   return (
     <NavigationContainer>
       {isAuthenticated ? (
-        <AppNavigator />
+        <AppNavigator userId={userId} nickname={nickname} />
       ) : (
-        <AuthAnonymousScreen onAuthSuccess={() => setIsAuthenticated(true)} />
+        <AuthAnonymousScreen
+          onAuthSuccess={(uid: string, nick: string) => {
+            setUserId(uid);
+            setNickname(nick);
+            setIsAuthenticated(true);
+          }}
+        />
       )}
     </NavigationContainer>
   );
